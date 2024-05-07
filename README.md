a2fe33ee4f1310844f54b87d83ea9366c1209a00
Israel Balbuena <israel.balbuena@alumnos.cide.edu>
2024-05-07 12:54:34 -0600

    end up version

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Getting-and-Cleaning-Data
+
+This repo is the "Getting and Cleaning Data" Peer-graded from Johns Hopkins Data Specialization. Its goal is creating a database from test and train databases of "Smartphone-Based Recognition of Human Activities and Postural Transitions" Samsung´s experiment, through cleaning, reading and analyzing its files. 
+
+How to start?
+
+The original databases, given by Cousera Platform, own a mistake which needs to be resolved before reading the files with R: some records in "x_test" and "y_train" files use double spaces as their delimiter and another use a simple space as their delimiter. 
+
+So, before reading the files with R, double spaces were manually replaced with simple spaces using "Replace All" function from txt editor. This step was the most important part due to the origin error would made that "x_test" and "y_test" were read as they had more than 561 features(variables), situation which can´t be possible in accordance with the list of features given by the course.
+
+Reading Data
+
+After cleaning databases, test and train databases were read separately, as it can be reviewed in "Building Test Part" and "Building Train Part", by using read_delim function from dplyr package and using simple space " " as the delimiter. These databases were joined with the features names, subjects who carried out the experiment and the activity which they took part in.  
+
+Getting Tidy Data and some measurements
+
+Complete Data Base was created by joining test and train databases using "rbind" function from base package. After that, data was manipulated using dplyr package in such a way that were selected only features(variables) which were the average or the standard deviation as the assignment indicates. 
+
+Finally, using functions from dplyr package was obtained a new database which contains the average and standard deviation of each activity performed by each of the 30 participants. 
---
+++ run_analysis.R
@@ -0,0 +1,78 @@
+library(pacman)
+
+p_load(tidyverse)
+
+
+Sys.setlocale("LC_ALL", "es_ES.UTF-8") 
+
+options(scipen=999)
+
+features <- read_delim("Data/features.txt",col_names = F)
+
+################################################################################
+########################## BUILDING TEST PART ##################################
+################################################################################
+
+test_subject <- read_delim("Data/test/subject_test.txt",delim = ",", 
+                           col_names = "Subject")
+
+test_x <- read_delim("Data/test/X_test.txt",
+                     delim = " ", col_names = features$X2 )
+
+test_y <- read_delim("Data/test/y_test.txt",
+                     delim = ",", col_names = "Activity")
+
+test_data <- cbind(test_y,test_subject,test_x) 
+
+################################################################################
+########################## BUILDING TRAIN PART ##############################
+################################################################################
+
+training_subject <- read_delim("Data/train/subject_train.txt",delim = " ",
+                               col_names = "Subject")
+
+training_x <- read_delim("Data/train/X_train.txt", delim = " ", 
+                         col_names = features$X2 )
+
+training_y <- read_delim("Data/train/y_train.txt", delim = ",", 
+                         col_names = "Activity")
+
+training_data <- cbind(training_y,training_subject,training_x) 
+
+################################################################################
+################################ COMPLETE DATABASE #############################
+################################################################################
+
+complete_database <- rbind(test_data,training_data)
+
+################################################################################
+############ GETTING MEAN AND STANDARD DEVIATION FOR EACH MEASUREMENT #########
+################################################################################
+
+some_measurements <- complete_database %>% mutate(Activity_label =case_when(
+  Activity == 1 ~ "Walking",
+  Activity == 2 ~ "Walking_Upstairs",
+  Activity == 3 ~ "Walking_Downstairs",
+  Activity == 4 ~ "Sitting",
+  Activity == 5 ~ "Standing",
+  Activity == 6 ~ "Laying")) %>% 
+  relocate("Activity","Activity_label") %>% 
+  select( Activity_label, Subject,    
+          contains("mean()") | contains("std()"))
+
+################################################################################
+################################ FINAL TIDY DATABASE ###########################
+################################################################################
+
+averages <- some_measurements %>% group_by(Activity_label, Subject) %>% 
+  summarise_all(list(average = ~ mean(.,na.rm = T)))
+
+################################################################################
+################################################################################
+################################################################################
+
+
+write.csv(averages,"Tidy_Data.csv")
+
+
+
